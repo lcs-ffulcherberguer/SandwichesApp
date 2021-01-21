b579456f4711991a1ec9b78e2fb2dcd2ea3401eb
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-20 21:47:31 -0500

    Created a text, learned about 'comment + click' to add for example a HStack. I created a list and added  the assets needed.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            Image(systemName: "photo")
+            
+            
+          
+            VStack(alignment: .leading) {
+                Text("My sandwich!")
+                Text("3 ingredients")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                        
+                        
+                        
+            }
+            .frame(width: 0.0)
+            }
+        }
+            
     }
-}
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
