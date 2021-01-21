0dcc04d93085bf799f4dafcfa76d9c8b982d9527
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-20 22:40:06 -0500

    Slimed down the code by adding a struct. Added a new text (changed the colour for secondary and entered the text using 'Spacer()'. Added a new SwiftUI view called sandwich detail.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,23 +11,23 @@ struct ContentView: View {
     var sandwiches: [Sandwich] = []
     var body: some View {
         NavigationView{
-        List(sandwiches) { sandwich in
-            NavigationLink(destination: Text(sandwich.name)) {
-            Image(sandwich.thumbnailName)
-                .cornerRadius(8)
-            
-          
-            VStack(alignment: .leading) {
-                Text(sandwich.name)
-                Text("\(sandwich.ingredientCount)ingredients")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
+            List {
+               ForEach(sandwiches) { sandwich in
+               SandwichCell(sandwich: sandwich)
+                
+                
+               }
+             
+                HStack {
+                    //Make it centered
+                    Spacer()
+                    Text("\(sandwiches.count) Sandwiches")
+                        .foregroundColor(.secondary)
+                    Spacer()
                     
-                        
-                        
-                        }
-                    }
                 }
+                
+            }
             
         .navigationTitle("Sandwiches")
             
@@ -45,3 +45,25 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
+
+struct SandwichCell: View {
+    var sandwich: Sandwich
+    
+    var body: some View {
+        NavigationLink(destination: Text(sandwich.name)) {
+            Image(sandwich.thumbnailName)
+                .cornerRadius(8)
+            
+            
+            VStack(alignment: .leading) {
+                Text(sandwich.name)
+                Text("\(sandwich.ingredientCount)ingredients")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                
+                
+            }
+        }
+    }
+}
